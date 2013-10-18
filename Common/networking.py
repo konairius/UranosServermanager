d3795d041e5449a2782b5d306180c47629e92f1c
Konstantin Renner <rennerkonsti@gmail.com>
2013-10-18 11:02:13 +0200

    Added SSHKey for ssh authentication

--- Common/networking.py
+++ Common/networking.py
@@ -6,6 +6,17 @@ from Common.models import Command, CommandResult
 __author__ = 'konsti'
 
 
+class Connection(object, metaclass=ABCMeta):
+    """
+    Baseclass for all connection wrappers,
+    must be implemented by all connections
+    """
+
+    @abstractmethod
+    def execute(self, command: Command) -> CommandResult:
+        pass
+
+
 class IPConnection(object, metaclass=ABCMeta):
     """
     This a wrapper to enable abstract network communication,
@@ -23,6 +34,29 @@ class IPConnection(object, metaclass=ABCMeta):
         pass
 
 
+class SSHKey(object):
+    """
+    Container class for an ssh key
+    """
+
+    def __init__(self, public, private: str='', key_type: str='ssh-rsa'):
+        self._public = public
+        self._private = private
+        self._key_type = key_type
+
+    @property
+    def public(self):
+        return self.public
+
+    @property
+    def private(self):
+        return self.private
+
+    @property
+    def key_type(self):
+        return self._key_type
+
+
 class SSHConnection(IPConnection):
     """
     Wrapper for an SSH connection,
@@ -30,18 +64,25 @@ class SSHConnection(IPConnection):
     """
 
     _username = None
-    _private_key = None
+    _user_key = None
     _host_key = None
 
-    def __init__(self, ip: ip_address, port: int=22, username: str='', private_key: str='', host_key: str=''):
+    def __init__(self, username: str, user_key: SSHKey,
+                 host_key: SSHKey, ip: ip_address, port: int=22):
+        """
+        @param username: username on the remote machine
+        @param user_key: private key must be set in this case
+        @param host_key: private key can (and should) be empty
+        """
         super().__init__(ip, port)
         self._username = username
-        self._private_key = private_key
+        self._user_key = user_key
         self._host_key = host_key
 
     def _get_open_ssh_client(self) -> SSHClient:
         client = SSHClient()
-        client.get_host_keys().add(self._ip.compressed, 'ssh-rsa', PKey(data=self._host_key))
+        client.get_host_keys().add(self._ip.compressed, self._host_key.key_type, PKey(data=self._host_key.public))
+        return client
 
     def execute(self, command: Command) -> CommandResult:
         pass
