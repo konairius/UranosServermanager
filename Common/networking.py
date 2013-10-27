affa132064b8813269d671ecbe39b68b6aab1788
Konstantin Renner <rennerkonsti@gmail.com>
2013-10-27 17:30:17 +0100

    Added function to ping an ip (using os ping to avoid needing root) Added function to get the mac from an ip address (only works in local subnet, uses arp, updates arp if the target is not found)

--- Common/networking.py
+++ Common/networking.py
@@ -1,12 +1,48 @@
 from abc import ABCMeta, abstractmethod
-from netaddr import EUI, IPAddress
+from ipaddress import ip_address
+from locale import getlocale
+import re
+from subprocess import Popen, PIPE
 from Common.models import Command, CommandResult
 
 __author__ = 'konsti'
 
 
-def get_mac_address(ip: IPAddress) -> EUI:
-    pass
+def ping(ip: ip_address, count: int=1) -> float:
+    """
+    Generator for a dict containing the output of ping in a usable fashion
+    """
+    pid = Popen(['ping', '-c ' + str(count), str(ip)], stdout=PIPE)
+    while True:
+        next_line = pid.stdout.readline().decode(getlocale()[1])
+        if pid.poll() is not None and next_line == '':
+            break
+        if 'bytes from' in next_line:
+            result_string = next_line.split(':')[1].strip()[:-3]
+            yield dict(item.split('=') for item in result_string.split(' '))
+
+
+def _get_mac_from_arp(ip: ip_address) -> str:
+    """
+    @return standard unix encoded mac address
+    """
+    pid = Popen(['arp', '-n', str(ip)], stdout=PIPE)
+    s = pid.communicate()[0].decode(getlocale()[1])
+    mac = re.search(r"(([a-f\d]{1,2}:){5}[a-f\d]{1,2})", s).groups()[0]
+    if mac == '':
+        raise KeyError('{ip}s is not in arp Table' % {'ip': ip})
+    return mac
+
+
+def get_mac_address(ip: ip_address) -> str:
+    """
+    @return standard unix encoded mac address
+    """
+    try:
+        return _get_mac_from_arp(ip)
+    except KeyError:
+        ping(ip)
+        return _get_mac_from_arp(ip)
 
 
 class Computer(object):
