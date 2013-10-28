18dafe95a33aa8c89f22e7d852269fd5f0e8e680
Konstantin Renner <rennerkonsti@gmail.com>
2013-10-28 14:04:50 +0100

    Added arping mac determination as fallback

--- Common/networking.py
+++ Common/networking.py
@@ -3,7 +3,7 @@ from abc import ABCMeta, abstractmethod
 from ipaddress import ip_address
 from locale import getlocale
 import re
-from subprocess import Popen, PIPE
+from subprocess import Popen, PIPE, DEVNULL
 from warnings import warn
 from Common.models import Command, CommandResult
 
@@ -28,7 +28,7 @@ def ping(ip: ip_address, count: int=1, sync: bool=False) -> dict:
 
 def _get_mac_from_arp(ip: ip_address) -> str:
     """
-    @return standard unix encoded mac address
+    @return:standard unix encoded mac address
     """
     pid = Popen(['arp', '-n', str(ip)], stdout=PIPE)
     s = pid.communicate()[0].decode(getlocale()[1])
@@ -40,15 +40,35 @@ def _get_mac_from_arp(ip: ip_address) -> str:
     return mac
 
 
+def _get_mac_from_arping(ip: ip_address) -> str:
+    """
+    Much slower than even using arp then ping and arp again,
+    but it is a fallback.
+    @return:standard unix encoded mac address
+    """
+    pid = Popen(['arping', '-c1', str(ip)], stdout=PIPE, stderr=DEVNULL)
+    s = pid.communicate()[0].decode(getlocale()[1])
+    mac = re.search(r"(([A-F\d]{1,2}:){5}[A-F\d]{1,2})", s)
+    if mac is None:
+        raise KeyError('%(ip)s could not be found' % {'ip': ip})
+    mac = mac.groups()[0]
+    if mac == '':
+        raise KeyError('%(ip)s could not be found' % {'ip': ip})
+    return mac
+
+
 def get_mac_address(ip: ip_address) -> str:
     """
-    @return standard unix encoded mac address
+    @return: standard unix encoded mac address
     """
     try:
         return _get_mac_from_arp(ip)
     except KeyError:
         ping(ip, count=1, sync=True)
-        return _get_mac_from_arp(ip)
+        try:
+            return _get_mac_from_arp(ip)
+        except KeyError:
+            return _get_mac_from_arping(ip)
 
 
 class Computer(object):
@@ -57,29 +77,38 @@ class Computer(object):
 
     def __init__(self, host: str):
         """
-        @host Can be either the hostname or the ipaddress
+        @param host: Can be either the hostname or the ipaddress as a string
         """
         self.host = host
         self.last_ip = self.ip
         try:
-            self.mac = get_mac_address(self.ip)
+            self.last_mac = get_mac_address(self.ip)
         except KeyError:
             warn('MAC Address could not be determined, you will have to set it manually for some Commands to work',
                  RuntimeWarning)
 
     @property
-    def ip(self):
+    def ip(self) -> ip_address:
         """
         Tries to give you the current ip of the host,
         if it could not be determined, the latest known ip is returned.
         """
         try:
-            return ip_address(gethostbyname(self.host))
+            self.last_ip = ip_address(gethostbyname(self.host))
         except TimeoutError:
-            return self.last_ip
+            pass
+        return self.last_ip
 
     @property
-    def status(self):
+    def mac(self) -> str:
+        try:
+            self.last_mac = get_mac_address(self.ip)
+        except KeyError:
+            pass
+        return self.last_mac
+
+    @property
+    def status(self) -> 'UP' or 'DOWN':
         try:
             ping(self.ip)
             return 'UP'
