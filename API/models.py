ce421425507633933e540d565d342981ed5908bf
Konstantin Renner <rennerkonsti@gmail.com>
2013-11-20 13:36:44 +0100

    Moved User tp API

---
+++ API/models.py
@@ -0,0 +1,48 @@
+from collections import defaultdict
+
+__author__ = 'konsti'
+
+
+class User(object):
+    _latest_id = 0
+    _users = dict()
+    _permission_map = defaultdict(False)
+
+    #noinspection PyAttributeOutsideInit
+    @classmethod
+    def reset_users(cls):
+        cls._latest_id = 0
+        cls._users = dict()
+
+    @classmethod
+    def get_next_id(cls) -> int:
+        cls._latest_id += 1
+        return cls._latest_id
+
+    @classmethod
+    def add_user(cls, user):
+        if user.id in cls._users.keys():
+            raise KeyError(
+                'The UserId %(id)i is already given to %(current)s' % {'id': user.id, 'current': cls._users[user.id]})
+        cls._users[user.id] = user
+
+    def __init__(self, name: str):
+        self._name = name
+        self._id = self.get_next_id()
+        self.add_user(self)
+
+    def __str__(self):
+        return self._name
+
+    def __repr__(self):
+        return str(self._id)
+
+    @property
+    def id(self):
+        return self._id
+
+    def add_permission(self, permission):
+        self._permission_map[permission] = True
+
+    def remove_permission(self, permission):
+        self._permission_map[permission] = False
--- Common/models.py
+++ Common/models.py
@@ -1,45 +1,8 @@
 from abc import ABCMeta, abstractmethod
 from datetime import datetime
+from API.models import User
 
 __author__ = 'konsti'
-
-
-class User(object):
-    _latest_id = 0
-    _users = dict()
-
-    #noinspection PyAttributeOutsideInit
-    @classmethod
-    def reset_users(cls):
-        cls._latest_id = 0
-        cls._users = dict()
-
-    @classmethod
-    def get_next_id(cls) -> int:
-        cls._latest_id += 1
-        return cls._latest_id
-
-    @classmethod
-    def add_user(cls, user):
-        if user.id in cls._users.keys():
-            raise KeyError(
-                'The UserId %(id)i is already given to %(current)s' % {'id': user.id, 'current': cls._users[user.id]})
-        cls._users[user.id] = user
-
-    def __init__(self, name: str):
-        self._name = name
-        self._id = self.get_next_id()
-        self.add_user(self)
-
-    def __str__(self):
-        return self._name
-
-    def __repr__(self):
-        return str(self._id)
-
-    @property
-    def id(self):
-        return self._id
 
 
 class Time(object):
